5194ea9db260f26342a0f5e38905f58b33a2722f
thalazyindian <satyabrat.me@gmail.com>
2017-09-29 12:00:13 +0000

    msm8916-comon: Update seccomp policy from CAF

--- product/qcom-seccomp.mk
+++ product/qcom-seccomp.mk
@@ -1,3 +1,4 @@
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
